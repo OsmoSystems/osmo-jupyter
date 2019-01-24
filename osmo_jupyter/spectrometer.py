77e5c3ac963ecec49980196869b5c58ae321b48a
Unknown <jacob.r.goldberg@gmail.com>
2019-01-23 17:07:12 -0800

    spectrometer functions: implemented suggestions from review

--- osmo_jupyter/spectrometer.py
+++ osmo_jupyter/spectrometer.py
@@ -2,70 +2,75 @@ import pandas as pd
 import io
 
 
+SPECTRAL_DATA_IDENTIFIER = '>>>>>Begin Spectral Data<<<<<'
+
+
 def _import_spectrometer_txt(spectrometer_data_file_path):
+    # The raw spectrometer file contains mixed "\r\n" and "\n" line endings
+    # Open file with newline=None to ensure all line endings are "\n"
     with open(spectrometer_data_file_path, 'r', newline=None) as infile:
+        # Read through file metadata until we find the marker that identifies the start of the tab-delimited data
         for line in infile:
-            if line.strip() == '>>>>>Begin Spectral Data<<<<<':
-                file_content_with_unix_endings = infile.read()
-                unformatted_spectrometer_data = pd.read_csv(
-                    io.StringIO(file_content_with_unix_endings),
+            if line.strip() == SPECTRAL_DATA_IDENTIFIER:
+                unformatted_spectrometer_df = pd.read_csv(
+                    io.StringIO(infile.read()),
                     sep='\t',
                     lineterminator='\n',
                 )
 
-                return unformatted_spectrometer_data
+                return unformatted_spectrometer_df
         raise ValueError('Spectrometer data not in expected format; check the file header')
 
 
-def _clean_up_spectrometer_data_header(unformatted_spectrometer_data):
+def _clean_up_spectrometer_df_header(unformatted_spectrometer_df):
 
-    ''' Cleans up header of raw spectrometer data, and removes redundant timestamp column.
+    ''' Cleans up header of raw spectrometer df, and removes redundant timestamp column.
 
     Args:
-        unformatted_spectrometer_data: a pandas df of original spectrometer data,
+        unformatted_spectrometer_df: a pandas df of original spectrometer data,
             imported with _import_spectrometer_txt()
 
     Return:
-        A pandas dataframe with a sensible header, and only one timestamp column
+        A pandas df with a sensible header, and only one timestamp column
     '''
 
-    timestamp_column, epoch_column = unformatted_spectrometer_data.columns[[0, 1]]
-    spectrometer_data_with_clean_header = unformatted_spectrometer_data.rename(
+    timestamp_column, epoch_column = unformatted_spectrometer_df.columns[[0, 1]]
+    spectrometer_df_with_clean_header = unformatted_spectrometer_df.rename(
         # timestamp column is in local time, epoch_time column is in UTC
         columns={
             timestamp_column: 'timestamp',
             epoch_column: 'epoch_time'
         }
     )
-    # Remove redundant timestamp column
-    spectrometer_data_with_clean_header.drop(['epoch_time'], axis=1, inplace=True)
+    # 'epoch_time' column is redundant to 'timestamp' column. Remove it
+    spectrometer_df_with_clean_header.drop(['epoch_time'], axis=1, inplace=True)
 
-    return spectrometer_data_with_clean_header
+    return spectrometer_df_with_clean_header
 
 
-def _reformat_spectrometer_data(spectrometer_data_with_clean_header):
+def _reformat_spectrometer_df(spectrometer_df_with_clean_header):
     ''' Reformats a df of spectrometer data with many wavelength columns,to a more useful format with 2 columns
 
     Args:
-        spectrometer_data_with_clean_header: a pandas df of spectrometer data,
-            cleaned up with _clean_up_spectrometer_data_header()
+        spectrometer_df_with_clean_header: a pandas df of spectrometer data,
+            cleaned up with _clean_up_spectrometer_df_header()
 
     Return:
         A pandas df, indexed by timestamp, with 2 columns: wavelength and intensity
     '''
 
     n_timestamp_columns = 1
-    wavelength_columns = spectrometer_data_with_clean_header.columns[n_timestamp_columns:]
+    wavelength_columns = spectrometer_df_with_clean_header.columns[n_timestamp_columns:]
     # melt function is a pivot and turns 3648 columns, each one wavelength, into one column
-    reformatted_spectrometer_data = spectrometer_data_with_clean_header.melt(
+    reformatted_spectrometer_df = spectrometer_df_with_clean_header.melt(
         id_vars=['timestamp'],
         value_vars=wavelength_columns,
         var_name='wavelength',
         value_name='intensity'
     )
-    reformatted_spectrometer_data['wavelength'] = reformatted_spectrometer_data['wavelength'].astype(float)
+    reformatted_spectrometer_df['wavelength'] = reformatted_spectrometer_df['wavelength'].astype(float)
 
-    return reformatted_spectrometer_data.set_index(['timestamp'])
+    return reformatted_spectrometer_df.set_index(['timestamp'])
 
 
 def import_and_format_spectrometer_data(spectrometer_data_file_path):
@@ -76,20 +81,26 @@ def import_and_format_spectrometer_data(spectrometer_data_file_path):
             'Time Series (column data)'
 
     Return:
-        A pandas dataframe, indexed by timestamp, with 2 columns: wavelength and intensity
+        A pandas df, indexed by timestamp, with 2 columns: wavelength and intensity
     '''
-    unformatted_spectrometer_data = _import_spectrometer_txt(spectrometer_data_file_path)
-    spectrometer_data_with_clean_header = _clean_up_spectrometer_data_header(unformatted_spectrometer_data)
-    reformatted_spectrometer_data = _reformat_spectrometer_data(spectrometer_data_with_clean_header)
+    unformatted_spectrometer_df = _import_spectrometer_txt(spectrometer_data_file_path)
+    spectrometer_df_with_clean_header = _clean_up_spectrometer_df_header(unformatted_spectrometer_df)
+    reformatted_spectrometer_df = _reformat_spectrometer_df(spectrometer_df_with_clean_header)
 
-    return reformatted_spectrometer_data
+    return reformatted_spectrometer_df
 
 
-def spectrometer_intensity_summary(reformatted_spectrometer_data, minimum_wavelength=340, maximum_wavelength=1035):
+MIN_SPECTROMETER_WAVELENGTH = 340
+MAX_SPECTROMETER_WAVELENGTH = 1035
+
+
+def spectrometer_intensity_summary(reformatted_spectrometer_df,
+                                   minimum_wavelength=MIN_SPECTROMETER_WAVELENGTH,
+                                   maximum_wavelength=MAX_SPECTROMETER_WAVELENGTH):
     ''' Creates intensity summary (mean) of spectrometer data over some spectrum of wavelengths.
 
     Args:
-        reformatted_spectrometer_data: pandas df of spectrometer data formatted by import_and_format_spectrometer_data()
+        reformatted_spectrometer_df: pandas df of spectrometer data formatted by import_and_format_spectrometer_data()
         minimum_wavelength (optional): sets a lower bound for range over which wavelengths will be summarized
         maximum_wavelength (optional): sets an upper bound for range over which wavelengths will be summarized
 
@@ -98,10 +109,14 @@ def spectrometer_intensity_summary(reformatted_spectrometer_data, minimum_wavele
             data over some spectrum
     '''
 
-    relevant_wavelengths = reformatted_spectrometer_data['wavelength'].between(minimum_wavelength, maximum_wavelength)
-    filtered_spectrometer_data = reformatted_spectrometer_data[relevant_wavelengths]
+    relevant_wavelengths = reformatted_spectrometer_df['wavelength'].between(minimum_wavelength, maximum_wavelength)
+    filtered_spectrometer_data = reformatted_spectrometer_df[relevant_wavelengths]
 
-    # Question whether or not this should be mean. An integral may relate more closely to a useful unit
+    # Question whether or not this function should perform a mean for its summary. A mean, sum and integral all
+    # produce the same relative change over time, only with different units. An integral may provide us with a
+    # physically useful unit (radiance) but it seems that our spectrometer is not calibrated for measuring radiance,
+    # so this operation would likely give us a unit close to something useful, but technically arbitrary.
+    # Although mean provides arbitrary units, it makes data comparable to tests from the past, so keeping for now.
     intensity_summary = filtered_spectrometer_data.groupby(['timestamp']).mean().drop(['wavelength'], axis=1)
 
     return intensity_summary
--- osmo_jupyter/spectrometer_test.py
+++ osmo_jupyter/spectrometer_test.py
@@ -7,11 +7,51 @@ import pytest
 from . import spectrometer as module
 
 
+FORMATTED_SPECTROMETER_DF = pd.DataFrame({
+    'timestamp': [
+        '2019-01-07 16:13:37.597000',
+        '2019-01-07 16:13:42.397000',
+        '2019-01-07 16:13:47.496000',
+        '2019-01-07 16:13:37.597000',
+        '2019-01-07 16:13:42.397000',
+        '2019-01-07 16:13:47.496000',
+        '2019-01-07 16:13:37.597000',
+        '2019-01-07 16:13:42.397000',
+        '2019-01-07 16:13:47.496000'
+    ],
+    'wavelength': [
+        344.05,
+        344.05,
+        344.05,
+        1031.859,
+        1031.859,
+        1031.859,
+        1032.175,
+        1032.175,
+        1032.175,
+    ],
+    'intensity': [
+        -626.00,
+        -640.67,
+        -546.47,
+        627.00,
+        198.99,
+        -546.47,
+        -163.15,
+        -79.00,
+        189.10,
+    ],
+
+}).set_index('timestamp')
+
+
 class TestImportSpectrometerData:
     def test_import_spectrometer_txt(self):
-        test_data_file_path = pkg_resources.resource_filename('osmo_jupyter', 'mock_spectrometer_data.txt')
-        test_spectrometer_data = module._import_spectrometer_txt(test_data_file_path)
-        mock_spectrometer_data = pd.DataFrame({
+        test_data_file_path = pkg_resources.resource_filename('osmo_jupyter',
+                                                              'test_fixtures/mock_spectrometer_data.txt'
+                                                              )
+        test_spectrometer_df = module._import_spectrometer_txt(test_data_file_path)
+        mock_spectrometer_df = pd.DataFrame({
             'Unnamed: 0': ['2019-01-07 16:13:37.597000', '2019-01-07 16:13:42.397000', '2019-01-07 16:13:47.496000'],
             'Unnamed: 1': [1546906417597, 1546906422397, 1546906427496],
             '344.05': [-626.00, -640.67, -546.47],
@@ -19,174 +59,122 @@ class TestImportSpectrometerData:
             '1032.175': [-163.15, -79.00, 189.10]
         })
 
-        pd.testing.assert_frame_equal(mock_spectrometer_data, test_spectrometer_data)
+        pd.testing.assert_frame_equal(mock_spectrometer_df, test_spectrometer_df)
 
     def test_broken_spectrometer_file(self):
-        test_data_file_path = pkg_resources.resource_filename('osmo_jupyter', 'mock_bad_spectrometer_data.txt')
-
+        test_data_file_path = pkg_resources.resource_filename('osmo_jupyter',
+                                                              'test_fixtures/mock_bad_spectrometer_data.txt'
+                                                              )
         with pytest.raises(ValueError):
             module._import_spectrometer_txt(test_data_file_path)
 
 
-def test_clean_up_spectrometer_data_header():
-    mock_unformatted_spectrometer_data = pd.DataFrame({
+def test_clean_up_spectrometer_df_header():
+    mock_unformatted_spectrometer_df = pd.DataFrame({
         'timestamp_with_wrong_name': [sentinel.timestamp],
         'epoch_time_with_wrong_name': [sentinel.epoch_timestamp],
         'wavelength_intensity': [sentinel.intensity]
     })
 
-    expected_spectrometer_data_with_clean_header = pd.DataFrame({
+    expected_spectrometer_df_with_clean_header = pd.DataFrame({
         'timestamp': [sentinel.timestamp],
         'wavelength_intensity': [sentinel.intensity]
     })
-    actual = module._clean_up_spectrometer_data_header(mock_unformatted_spectrometer_data)
+    actual = module._clean_up_spectrometer_df_header(mock_unformatted_spectrometer_df)
 
-    pd.testing.assert_frame_equal(actual, expected_spectrometer_data_with_clean_header)
+    pd.testing.assert_frame_equal(actual, expected_spectrometer_df_with_clean_header)
 
 
-def test_reformat_spectrometer_data():
-    mock_unformatted_spectrometer_data = pd.DataFrame({
+def test_reformat_spectrometer_df():
+    mock_unformatted_spectrometer_df = pd.DataFrame({
         'timestamp': ['2019-01-07 16:13:37.597000', '2019-01-07 16:13:42.397000', '2019-01-07 16:13:47.496000'],
         '344.05': [-626.00, -640.67, -546.47],
         '1031.859': [627.00, 198.99, -546.47],
         '1032.175': [-163.15, -79.00, 189.10]
     })
-    actual = module._reformat_spectrometer_data(mock_unformatted_spectrometer_data)
-    expected_formatted_spectrometer_data = pd.DataFrame({
-        'timestamp': [
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000'
-        ],
-        'wavelength': [
-            344.05,
-            344.05,
-            344.05,
-            1031.859,
-            1031.859,
-            1031.859,
-            1032.175,
-            1032.175,
-            1032.175,
-        ],
-        'intensity': [
-            -626.00,
-            -640.67,
-            -546.47,
-            627.00,
-            198.99,
-            -546.47,
-            -163.15,
-            -79.00,
-            189.10,
-        ],
-
-    }).set_index('timestamp')
-    pd.testing.assert_frame_equal(actual, expected_formatted_spectrometer_data)
+    actual = module._reformat_spectrometer_df(mock_unformatted_spectrometer_df)
+    pd.testing.assert_frame_equal(actual, FORMATTED_SPECTROMETER_DF)
 
 
 def test_import_and_format_spectrometer_data():
-    test_data_file_path = pkg_resources.resource_filename('osmo_jupyter', 'mock_spectrometer_data.txt')
+    test_data_file_path = pkg_resources.resource_filename('osmo_jupyter',
+                                                          'test_fixtures/mock_spectrometer_data.txt'
+                                                          )
     actual = module.import_and_format_spectrometer_data(test_data_file_path)
-    expected_formatted_spectrometer_data = pd.DataFrame({
-        'timestamp': [
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000'
-        ],
-        'wavelength': [
-            344.05,
-            344.05,
-            344.05,
-            1031.859,
-            1031.859,
-            1031.859,
-            1032.175,
-            1032.175,
-            1032.175,
-        ],
-        'intensity': [
-            -626.00,
-            -640.67,
-            -546.47,
-            627.00,
-            198.99,
-            -546.47,
-            -163.15,
-            -79.00,
-            189.10,
-        ],
-
-    }).set_index('timestamp')
-
-    pd.testing.assert_frame_equal(actual, expected_formatted_spectrometer_data)
+    pd.testing.assert_frame_equal(actual, FORMATTED_SPECTROMETER_DF)
 
 
-def test_spectrometer_intensity_summary():
-    sample_formatted_spectrometer_data = pd.DataFrame({
-        'timestamp': [
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000'
-        ],
-        'wavelength': [
-            344.05,
-            344.05,
-            344.05,
-            1031.859,
-            1031.859,
-            1031.859,
-            1032.175,
-            1032.175,
-            1032.175,
-        ],
-        'intensity': [
-            -626.00,
-            -640.67,
-            -546.47,
-            627.00,
-            198.99,
-            -546.47,
-            -163.15,
-            -79.00,
-            189.10,
-        ],
-    }).set_index('timestamp')
-
+@pytest.mark.parametrize(
+    "minimum_wavelength, maximum_wavelength, expected_intensity_summary",
+    [(344, 1032, pd.DataFrame({
+                                'timestamp': [
+                                    '2019-01-07 16:13:37.597000',
+                                    '2019-01-07 16:13:42.397000',
+                                    '2019-01-07 16:13:47.496000',
+                                ],
+                                'intensity': [
+                                    0.5,
+                                    -220.84,
+                                    -546.47,
+                                ]
+                                }).set_index('timestamp')),
+     (None, 500, pd.DataFrame({
+                               'timestamp': [
+                                   '2019-01-07 16:13:37.597000',
+                                   '2019-01-07 16:13:42.397000',
+                                   '2019-01-07 16:13:47.496000',
+                               ],
+                               'intensity': [
+                                   -626.0,
+                                   -640.67,
+                                   -546.47,
+                               ]
+                               }).set_index('timestamp')),
+     (1032, None, pd.DataFrame({
+                                'timestamp': [
+                                    '2019-01-07 16:13:37.597000',
+                                    '2019-01-07 16:13:42.397000',
+                                    '2019-01-07 16:13:47.496000',
+                                ],
+                                'intensity': [
+                                    -163.15,
+                                    -79.0,
+                                    189.1,
+                                ]
+                                }).set_index('timestamp')),
+     (None, None, pd.DataFrame({
+                                 'timestamp': [
+                                     '2019-01-07 16:13:37.597000',
+                                     '2019-01-07 16:13:42.397000',
+                                     '2019-01-07 16:13:47.496000',
+                                 ],
+                                 'intensity': [
+                                     -54.05,
+                                     -173.56,
+                                     -301.28,
+                                     ]
+                                 }).set_index('timestamp')),
+     ]
+)
+def test_spectrometer_intensity_summary(minimum_wavelength, maximum_wavelength, expected_intensity_summary):
     actual = module.spectrometer_intensity_summary(
-        sample_formatted_spectrometer_data,
-        minimum_wavelength=344,
-        maximum_wavelength=1032
+        FORMATTED_SPECTROMETER_DF,
+        minimum_wavelength=minimum_wavelength,
+        maximum_wavelength=maximum_wavelength
     )
-    expected_high_wavelength_intensity_summary = pd.DataFrame({
-        'timestamp': [
-            '2019-01-07 16:13:37.597000',
-            '2019-01-07 16:13:42.397000',
-            '2019-01-07 16:13:47.496000',
-        ],
-        'intensity': [
-            0.5,
-            -220.84,
-            -546.47,
-        ]
-    }).set_index('timestamp')
+    if minimum_wavelength is None:
+        actual = module.spectrometer_intensity_summary(
+            FORMATTED_SPECTROMETER_DF,
+            maximum_wavelength=maximum_wavelength
+        )
+    if maximum_wavelength is None:
+        actual = module.spectrometer_intensity_summary(
+            FORMATTED_SPECTROMETER_DF,
+            minimum_wavelength=minimum_wavelength
+        )
+    if maximum_wavelength is None and minimum_wavelength is None:
+        actual = module.spectrometer_intensity_summary(
+            FORMATTED_SPECTROMETER_DF,
+        )
 
-    pd.testing.assert_frame_equal(actual, expected_high_wavelength_intensity_summary)
+    pd.testing.assert_frame_equal(actual, expected_intensity_summary)
