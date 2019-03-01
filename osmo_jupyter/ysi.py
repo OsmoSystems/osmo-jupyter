8eeff9c8cebaf4906a1b77b64cb53b04c1c431f5
Jaime <jaime@osmobot.com>
2019-02-28 17:08:10 -0800

    docstring wordsmithing

--- osmo_jupyter/ysi.py
+++ osmo_jupyter/ysi.py
@@ -31,14 +31,13 @@ def join_nearest_ysi_data(
 ):
     '''
     Params:
-        other_data: DataFrame with a timestamp column to be augmented with YSI data
+        other_data: DataFrame (with a timestamp column) to be augmented with YSI data
         ysi_data: DataFrame from YSI. Should be indexed by pre-parsed timestamps. Get this with, e.g.:
             pd.read_csv(
                 <YSI filename>,
                 parse_dates=['Timestamp']
             ).set_index('Timestamp')
-        other_data_timestamp_column: column name in other_data containing pre-parsed timestamps.
-            Default: 'timestamp'. Pass None to use the index.
+        other_data_timestamp_column: Default: 'timestamp'. Column name in other_data containing pre-parsed timestamps.
     Return:
         DataFrame with each row in other_data augmented with the closest-timestamp data from the YSI.
         Discards "other_data" that is collected outside of the timerange of the YSI data.
