8381a76b3eea3eee1bc5e1fc35b73bc5d89a3aab
Jaime <jaime@osmobot.com>
2019-03-07 09:53:55 -0800

    Delete old plot_test.py file

--- osmo_jupyter/plot_test.py
+++
@@ -1,9 +0,0 @@
-class TestImport:
-    def test_import(self):
-        from osmo_jupyter.plot import get_scatter, get_rgb_scatters, get_rgb_columns, add_rgb_columns, get_layout_with_annotations  # noqa
-        # subsample_for_plot()
-        # get_scatter()
-        # get_rgb_scatters()
-        # get_rgb_columns()
-        # add_rgb_columns()
-        # get_layout_with_annotations()
