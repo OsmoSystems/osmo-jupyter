be26444d48b5c8bd9a5abdbb7b453c84e101af33
Jaime <jaimemarijke@gmail.com>
2019-03-11 16:31:19 -0700

    Added heatmapify function + tests (#15)
    * Added heatmapify function + tests
    
    * Revert mistaken gitignore changes
    
    * .gitignore empty end line
    
    * Add test for order within blocks
    
    * Some reformatting to try to make block slicing clearer
    
    * Allow configuration of averaging function
    
    * Clarify docstrings about averaging_function

---
+++ osmo_jupyter/plot/heatmap.py
@@ -0,0 +1,115 @@
+import numpy as np
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+
+
+def cut_array2d(array, block_shape):
+    ''' Cuts up an array into blocks of shape `block_shape`. If `block_shape` doesn't factor perfectly into the
+        shape of the array, drops data from the right and bottom edges of the input `array`.
+        Heavily, heavily adapted from https://stackoverflow.com/a/17385776/1701416
+
+    Args:
+        array: a 2D numpy array
+        block_shape: a 2-tuple. The shape of the blocks you want the `array` to be cut into
+
+    Return:
+        (block_centers, blocks) tuple where
+            block_centers: a 1D array of 2-tuple coordinates for the centers of the sliced blocks
+            blocks: a 1D array of blocks of shape `block_shape`
+    '''
+    array_height, array_width = array.shape
+    block_height, block_width = block_shape
+
+    # Think, gridlines:
+    ycut = np.arange(0, array_height + 1, block_height)
+    xcut = np.arange(0, array_width + 1, block_width)
+
+    block_centers = [
+        (
+            (ycut[i] + ycut[i+1] - 1) / 2,
+            (xcut[j] + xcut[j+1] - 1) / 2
+        )
+        for i, _ in enumerate(ycut[:-1])
+        for j, _ in enumerate(xcut[:-1])
+    ]
+
+    blocks = [
+        array[ycut[i]:ycut[i+1], xcut[j]:xcut[j+1]]
+        for i, _ in enumerate(ycut[:-1])
+        for j, _ in enumerate(xcut[:-1])
+    ]
+
+    return block_centers, blocks
+
+
+def get_block_means_2d(block_centers, blocks, averaging_function=np.mean):
+    ''' Given 1D arrays of block_centers and blocks, construct a 2D array of block means.
+
+    Args:
+        block_centers: a 1D array of 2-tuple coordinates for the centers of the sliced blocks
+        blocks: a 1D array of 2D blocks
+        averaging_function: Optional (default=np.mean). Function to use to get the average of each block (a 2D array)
+
+    Returns:
+        a 2D array of block means
+    '''
+    block_means = [averaging_function(block) for block in blocks]
+
+    # block_centers is a list of (x,y) tuples: pixel coordinates of the blocks within the image
+    # We don't actually care about the pixel coordinates - just the number of blocks in each row and column
+    block_centers_x, block_centers_y = zip(*block_centers)
+    blocks_height = len(set(block_centers_x))
+    blocks_width = len(set(block_centers_y))
+
+    # Construct a new 2D array that just contains the mean value of each block
+    block_means_2d = np.reshape(
+        block_means,
+        newshape=(blocks_height, blocks_width)
+    )
+
+    return block_means_2d
+
+
+def display_heatmap(array, display_decimals=2, title='Heatmap'):
+    ''' Builds the heatmap figure from provided array
+
+    Args:
+        array: A 2D numpy array to render as a heatmap
+        display_decimals: Optional (default=2). the number of decimals to round to in the annotation
+        title: Optional (default='Heatmap')
+
+    Returns:
+        A heatmap Figure.
+        Can be displayed in a jupyter notebook using the display() function.
+    '''
+    heatmap = ff.create_annotated_heatmap(
+        array,
+        annotation_text=np.round(array, display_decimals),
+    )
+
+    heatmap.layout.yaxis.autorange = 'reversed'
+    heatmap.layout.title = title
+
+    return go.FigureWidget(heatmap)
+
+
+def heatmapify(array, averaging_function=np.mean, block_shape=(50, 50), display_decimals=2, title='Heatmap'):
+    ''' Constructs a heatmap from a 2D array. The heatmap is created by slicing the array into blocks the size of
+        `block_shape`, and then averaging each block.
+
+    Args:
+        array: A 2D numpy array to render as a heatmap. e.g. a single channel of an RGB image:
+            array = image[:, :, channel]  # channel is 0, 1, or 2, for r, g, b respectively
+        averaging_function: Optional (default=np.mean). Function to use to get the average of each block (a 2D array)
+        block_shape: Optional (default=(50,50)). The shape of block to group by and average over to create the heatmap
+        display_decimals: Optional (default=2). The number of decimals to round to in the annotation
+        title: Optional (default='Heatmap').
+
+    Returns:
+        A heatmap Figure.
+        Can be displayed in a jupyter notebook using the display() function.
+    '''
+    block_centers, blocks = cut_array2d(array, block_shape)
+    block_means_2d = get_block_means_2d(block_centers, blocks, averaging_function)
+
+    return display_heatmap(block_means_2d, display_decimals, title)
---
+++ osmo_jupyter/plot/heatmap_test.py
@@ -0,0 +1,213 @@
+from unittest.mock import sentinel
+
+import numpy as np
+import pytest
+
+from . import heatmap as module
+
+
+class TestCutArray2D:
+    def test_cut_odd_shape(self):
+        odd_array = np.array([
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        ])
+
+        expected_block_centers = [(1, 1), (1, 4), (1, 7)]
+        expected_blocks = np.array([
+            [
+                [1, 1, 1],
+                [1, 1, 1],
+                [1, 1, 1],
+            ],
+            [
+                [2, 2, 2],
+                [2, 2, 2],
+                [2, 2, 2],
+            ],
+            [
+                [3, 3, 3],
+                [3, 3, 3],
+                [3, 3, 3],
+            ],
+        ])
+
+        actual_block_centers, actual_blocks = module.cut_array2d(odd_array, block_shape=(3, 3))
+
+        np.testing.assert_array_equal(actual_block_centers, expected_block_centers)
+        np.testing.assert_array_equal(actual_blocks, expected_blocks)
+
+    def test_cut_even_shape(self):
+        even_array = np.array([
+            [1, 1, 2, 2],
+            [1, 1, 2, 2],
+            [3, 3, 4, 4],
+            [3, 3, 4, 4],
+        ])
+
+        expected_block_centers = [(0.5, 0.5), (0.5, 2.5), (2.5, 0.5), (2.5, 2.5)]
+        expected_blocks = np.array([
+            [
+                [1, 1],
+                [1, 1],
+            ],
+            [
+                [2, 2],
+                [2, 2],
+            ],
+            [
+                [3, 3],
+                [3, 3],
+            ],
+            [
+                [4, 4],
+                [4, 4],
+            ],
+        ])
+
+        actual_block_centers, actual_blocks = module.cut_array2d(even_array, block_shape=(2, 2))
+
+        np.testing.assert_array_equal(actual_block_centers, expected_block_centers)
+        np.testing.assert_array_equal(actual_blocks, expected_blocks)
+
+    def test_mismatch_shape_drops_edges(self):
+        mismatch_shaped_array = np.array([
+            [1, 1, 2],
+            [1, 1, 2],
+            [2, 2, 2],
+        ])
+
+        actual_block_centers, actual_blocks = module.cut_array2d(mismatch_shaped_array, block_shape=(2, 2))
+
+        expected_block_centers = [(0.5, 0.5)]
+        expected_blocks = np.array([
+            [
+                [1, 1],
+                [1, 1]
+            ],
+        ])
+
+        np.testing.assert_array_equal(actual_block_centers, expected_block_centers)
+        np.testing.assert_array_equal(actual_blocks, expected_blocks)
+
+    def test_maintains_order_within_block(self):
+        mismatch_shaped_array = np.array([
+            [1, 2, 5, 6],
+            [3, 4, 7, 8],
+        ])
+
+        actual_block_centers, actual_blocks = module.cut_array2d(mismatch_shaped_array, block_shape=(2, 2))
+
+        expected_block_centers = [(0.5, 0.5), (0.5, 2.5)]
+        expected_blocks = np.array([
+            [
+                [1, 2],
+                [3, 4]
+            ],
+            [
+                [5, 6],
+                [7, 8]
+            ],
+        ])
+
+        np.testing.assert_array_equal(actual_block_centers, expected_block_centers)
+        np.testing.assert_array_equal(actual_blocks, expected_blocks)
+
+    def test_returns_nothing_if_block_size_larger_than_array(self):
+        small_array = np.array([
+            [1, 1, 1],
+            [1, 1, 1],
+        ])
+
+        actual_block_centers, actual_blocks = module.cut_array2d(small_array, block_shape=(3, 3))
+
+        np.testing.assert_array_equal(actual_block_centers, [])
+        np.testing.assert_array_equal(actual_blocks, [])
+
+
+class TestGetBlockMeans2D:
+    def test_gets_block_means(self):
+        actual = module.get_block_means_2d(
+            block_centers=[(0.5, 0.5), (0.5, 2.5)],
+            blocks=np.array([
+                [
+                    [1, 2],
+                    [3, 4],
+                ],
+                [
+                    [10, 20],
+                    [30, 40],
+                ]
+            ])
+        )
+
+        expected = np.array([[2.5, 25.0]])
+
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_reshapes_to_match_block_centers(self):
+        actual = module.get_block_means_2d(
+            block_centers=[(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3)],
+            blocks=[[1], [2], [3], [4], [5], [6]]
+        )
+
+        expected = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_uses_provided_averaging_function(self):
+        def _mock_averaging_func(array):
+            return sentinel.averaged
+
+        actual = module.get_block_means_2d(
+            block_centers=[(0.5, 0.5), (0.5, 2.5)],
+            blocks=np.array([
+                [
+                    [1, 2],
+                    [2, 3],
+                ],
+                [
+                    [10, 11],
+                    [11, 30],
+                ]
+            ]),
+            averaging_function=_mock_averaging_func
+        )
+
+        expected = np.array([[sentinel.averaged, sentinel.averaged]])
+
+        np.testing.assert_array_equal(actual, expected)
+
+    @pytest.mark.parametrize('name,block_centers,blocks', [
+        ('too many blocks', [(1, 1), (1, 2), (1, 3)], [[1], [2], [3], [4]]),
+        ('too many block centers', [(1, 1), (1, 2), (1, 3)], [[1], [2]]),
+        ('empty blocks', [(1, 1), (1, 2), (1, 3)], []),
+        ('empty block centers', [], [[1], [2], [3]]),
+    ])
+    def test_raises_on_mismatched_size(self, name, block_centers, blocks):
+        with pytest.raises(ValueError):
+            module.get_block_means_2d(block_centers, blocks)
+
+
+@pytest.fixture
+def mock_display_heatmap(mocker):
+    return mocker.patch.object(module, 'display_heatmap')
+
+
+class TestHeatmapify:
+    def test_heatmapify(self, mock_display_heatmap):
+        array = np.array([
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+            [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        ])
+
+        expected_block_means_2d = [[1.0, 2.0, 3.0]]
+
+        module.heatmapify(array, block_shape=(3, 3))
+
+        # call_args is a tuple of ((positional arguments), (kwargs))
+        actual_block_means_2d = mock_display_heatmap.call_args[0][0]
+
+        np.testing.assert_array_equal(actual_block_means_2d, expected_block_means_2d)
