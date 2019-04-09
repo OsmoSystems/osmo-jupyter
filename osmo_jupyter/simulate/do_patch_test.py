703f04c979b549cf1514a5ed1ed4f47002f5c593
Jaime <jaime@osmobot.com>
2019-04-08 17:57:24 -0700

    Use np.testing.assert_almost_equal

--- osmo_jupyter/simulate/do_patch_test.py
+++ osmo_jupyter/simulate/do_patch_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from osmo_jupyter.constants import TEMPERATURE_STANDARD_OPERATING_MIN, TEMPERATURE_STANDARD_OPERATING_MAX
@@ -32,20 +33,21 @@ class TestGetRateConstant:
     def test_get_rate_constant(self):
         # Spot-check rate function with some actual numbers
         actual = module._get_rate_constant(temperature_c=0, preexponential_factor=10, activation_energy=1)
-        assert actual == pytest.approx(9.995598)
+        np.testing.assert_almost_equal(actual, 9.9955975)
 
 
 class TestEstimateOpticalReading:
     @pytest.mark.parametrize(
         'name, do_pct_sat, temperature_c, expected',
         [
-            ('zero DO min temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 1.727900),
-            ('zero DO max temp', 0, TEMPERATURE_STANDARD_OPERATING_MAX, 1.268441),
-            ('max DO min temp', 100, TEMPERATURE_STANDARD_OPERATING_MIN, 0.625286),
-            ('max DO max temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0.396912),
+            ('zero DO min temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 1.7279002),
+            ('zero DO max temp', 0, TEMPERATURE_STANDARD_OPERATING_MAX, 1.2684410),
+            ('max DO min temp', 100, TEMPERATURE_STANDARD_OPERATING_MIN, 0.6252858),
+            ('max DO max temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0.3969120),
         ]
     )
     def test_estimate_optical_reading(self, name, do_pct_sat, temperature_c, expected):
-        # Spot-check function with some actual numbers
+        # Spot-check function with some actual numbers. This will of course change if we update the curve.
         actual = module._estimate_optical_reading(do_pct_sat, temperature_c)
         assert actual == pytest.approx(expected)
+        np.testing.assert_almost_equal(actual, expected)
