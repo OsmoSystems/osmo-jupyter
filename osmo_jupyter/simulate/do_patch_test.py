5ac993a9cd14a1b037dc517e74c17bd9f7dfe9df
thatneat <thatneat@gmail.com>
2019-04-29 11:44:47 -0700

    improve precision of ideal gas constant

--- osmo_jupyter/simulate/do_patch_test.py
+++ osmo_jupyter/simulate/do_patch_test.py
@@ -33,20 +33,20 @@ class TestGetRateConstant:
     def test_get_rate_constant(self):
         # Spot-check rate function with some actual numbers
         actual = module._get_rate_constant(temperature_c=0, preexponential_factor=10, activation_energy=1)
-        np.testing.assert_almost_equal(actual, 9.9955975)
+        np.testing.assert_almost_equal(actual, 9.9956, decimal=3)
 
 
 class TestEstimateOpticalReading:
     @pytest.mark.parametrize(
         'name, do_pct_sat, temperature_c, expected',
         [
-            ('zero DO min temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 1.7279002),
-            ('zero DO max temp', 0, TEMPERATURE_STANDARD_OPERATING_MAX, 1.2684410),
-            ('max DO min temp', 100, TEMPERATURE_STANDARD_OPERATING_MIN, 0.6252858),
-            ('max DO max temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0.3969120),
+            ('zero DO min temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 1.7279),
+            ('zero DO max temp', 0, TEMPERATURE_STANDARD_OPERATING_MAX, 1.2684),
+            ('max DO min temp', 100, TEMPERATURE_STANDARD_OPERATING_MIN, 0.6253),
+            ('max DO max temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0.3969),
         ]
     )
     def test_estimate_optical_reading(self, name, do_pct_sat, temperature_c, expected):
         # Spot-check function with some actual numbers. This will of course change if we update the curve.
         actual = module._estimate_optical_reading(do_pct_sat, temperature_c)
-        np.testing.assert_almost_equal(actual, expected)
+        np.testing.assert_almost_equal(actual, expected, decimal=3)
