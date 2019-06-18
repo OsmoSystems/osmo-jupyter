b5c67c0145dd42ec023721d43ed30f8d198e20d7
thatneat <thatneat@users.noreply.github.com>
2019-06-18 14:19:24 -0700

    DO -> mmHg, plus a couple of related notebook features (#28)
    * pull in couple of changes from notebook fork:
        * update DO unit to mmHG
        * don't round YSI temperature
        * add error bounds lines to `calibration_error_plot`
    * PR feedback
    * line length
    * PR feedback: find a less precise source
    * clarity - saturation fraction, not fraction saturation
    * use the actual middle DO

--- osmo_jupyter/simulate/do_patch_test.py
+++ osmo_jupyter/simulate/do_patch_test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from osmo_jupyter.constants import TEMPERATURE_STANDARD_OPERATING_MIN, TEMPERATURE_STANDARD_OPERATING_MAX
+from osmo_jupyter.constants import TEMPERATURE_STANDARD_OPERATING_MIN, TEMPERATURE_STANDARD_OPERATING_MAX, DO_MAX_MMHG,\
+    DO_MIN_MMHG
 import osmo_jupyter.simulate.do_patch as module
 
 
@@ -9,20 +10,22 @@ class TestNormalizedReading:
     @pytest.mark.parametrize(
         'name, do, temperature, expected_normalized_reading',
         [
-            ('max fluorescence at low DO, low temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 1),
-            ('min fluorescence at high DO, high temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0),
+            ('max fluorescence at low DO, low temp', DO_MIN_MMHG, TEMPERATURE_STANDARD_OPERATING_MIN, 1),
+            ('min fluorescence at high DO, high temp', DO_MAX_MMHG, TEMPERATURE_STANDARD_OPERATING_MAX, 0),
         ]
     )
     def test_normalized_reading_bounds(self, name, do, temperature, expected_normalized_reading):
         assert module.get_optical_reading_normalized(do, temperature) == expected_normalized_reading
 
+    middle_do = (DO_MIN_MMHG + DO_MAX_MMHG) / 2
+
     @pytest.mark.parametrize(
         'name, do, temperature',
         [
-            ('middlin DO at min temp', 50, TEMPERATURE_STANDARD_OPERATING_MIN),
-            ('middlin DO at max temp', 50, TEMPERATURE_STANDARD_OPERATING_MAX),
-            ('max temp at low DO', 0, TEMPERATURE_STANDARD_OPERATING_MAX),
-            ('min temp at max DO', 100, TEMPERATURE_STANDARD_OPERATING_MIN),
+            ('middlin DO at min temp', middle_do, TEMPERATURE_STANDARD_OPERATING_MIN),
+            ('middlin DO at max temp', middle_do, TEMPERATURE_STANDARD_OPERATING_MAX),
+            ('max temp at low DO', DO_MIN_MMHG, TEMPERATURE_STANDARD_OPERATING_MAX),
+            ('min temp at max DO', DO_MAX_MMHG, TEMPERATURE_STANDARD_OPERATING_MIN),
         ]
     )
     def test_normalized_reading_middlin_values_are_middlin(self, name, do, temperature):
@@ -31,15 +34,15 @@ class TestNormalizedReading:
 
 class TestEstimateOpticalReading:
     @pytest.mark.parametrize(
-        'name, do_pct_sat, temperature_c, expected',
+        'name, do_partial_pressure, temperature_c, expected',
         [
-            ('zero DO min temp', 0, TEMPERATURE_STANDARD_OPERATING_MIN, 4.4012),
-            ('zero DO max temp', 0, TEMPERATURE_STANDARD_OPERATING_MAX, 3.4646),
-            ('max DO min temp', 100, TEMPERATURE_STANDARD_OPERATING_MIN, 1.2577),
-            ('max DO max temp', 100, TEMPERATURE_STANDARD_OPERATING_MAX, 0.9995),
+            ('zero DO min temp', DO_MIN_MMHG, TEMPERATURE_STANDARD_OPERATING_MIN, 4.4012),
+            ('zero DO max temp', DO_MIN_MMHG, TEMPERATURE_STANDARD_OPERATING_MAX, 3.4646),
+            ('max DO min temp', DO_MAX_MMHG, TEMPERATURE_STANDARD_OPERATING_MIN, 1.261),
+            ('max DO max temp', DO_MAX_MMHG, TEMPERATURE_STANDARD_OPERATING_MAX, 1.002),
         ]
     )
-    def test_estimate_optical_reading(self, name, do_pct_sat, temperature_c, expected):
+    def test_estimate_optical_reading(self, name, do_partial_pressure, temperature_c, expected):
         # Spot-check function with some actual numbers. This will of course change if we update the curve.
-        actual = module._estimate_optical_reading(do_pct_sat, temperature_c)
+        actual = module._estimate_optical_reading(do_partial_pressure, temperature_c)
         np.testing.assert_almost_equal(actual, expected, decimal=3)
