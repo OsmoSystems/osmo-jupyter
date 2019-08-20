a44bb61955272e5d8ced7f426769768909f6eb91
Evan Simpson <EvanSimpson@users.noreply.github.com>
2019-08-20 10:20:19 -0400

    Add tools for processing calibration data (#33)
    * Add tools for processing calibration data
    
    * Add docstrings to dataset tools
    
    Also fixes picolog data to be upsampled per-file, rather than in bulk.
    
    * Clean up dataset equilibration filter
    
    * Address PR feedback for dataset tools
    
    * Remove mutable default args
    
    * Keep equilibrated data at end of dataset
    
    * Include all single point equilibration ranges

---
+++ osmo_jupyter/dataset.py
@@ -0,0 +1,300 @@
+import os
+from typing import List
+
+import pandas as pd
+
+
+def open_picolog_file(picolog_filepath: str) -> pd.DataFrame:
+    """
+        Open and parse a PicoLog data file such that the timestamps can be joined with calibration environment data.
+
+        Args:
+            picolog_filepath: A filepath to a PicoLog csv data file
+        Returns:
+            DataFrame of PicoLog file contents, with timestamp indexes and PicoLog prefix on all other columns,
+            upsampled and linearly interpolated to each intermediate second.
+    """
+    return (
+        pd.read_csv(
+            picolog_filepath,
+            index_col=0,  # first column, is unlabeled
+            parse_dates=[0],  # first column
+            date_parser=lambda col: pd.to_datetime(col, utc=False).tz_localize(
+                None
+            ),  # Remove timezone information
+        )
+        .rename_axis("timestamp")
+        .add_prefix("PicoLog ")
+        .resample("s")
+        .interpolate(method="slinear")
+    )
+
+
+def open_calibration_log_file(calibration_log_filepath: str) -> pd.DataFrame:
+    """
+        Open and parse a calibration environment data file such that the timestamps can be joined with PicoLog data.
+
+        Args:
+            calibration_log_filepath: A filepath to a calibration environment csv data log file.
+        Returns:
+            DataFrame of calibration data file contents, with timestamp indexes.
+    """
+    return pd.read_csv(
+        calibration_log_filepath,
+        index_col="timestamp",
+        parse_dates=["timestamp"],
+        date_parser=lambda col: pd.to_datetime(col, utc=False).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        ),  # Truncate fractional seconds
+    )
+
+
+def open_and_combine_picolog_and_calibration_data(
+    calibration_log_filepaths: List[str], picolog_log_filepaths: List[str]
+) -> pd.DataFrame:
+    """
+        Open and join a collection of PicoLog and calibration environment data files.
+
+        Args:
+            calibration_log_filepaths: A list of filepaths to calibration environment csv data log files.
+            picolog_log_filepaths: A list of filepaths to PicoLog csv data files.
+        Returns:
+            DataFrame of PicoLog data joined with matching calibration environment data.
+            PicoLog data is upsampled to 1 second increments and linearly interpolated
+    """
+    picolog_data = pd.concat(
+        [
+            open_picolog_file(picolog_filepath)
+            for picolog_filepath in picolog_log_filepaths
+        ],
+        sort=True,
+    )
+
+    calibration_data = pd.concat(
+        [
+            open_calibration_log_file(calibration_log_filepath)
+            for calibration_log_filepath in calibration_log_filepaths
+        ],
+        sort=True,
+    )
+
+    return calibration_data.join(  # By default join happens on index values
+        picolog_data, how="inner"  # Drop any rows without a match in both DataFrames
+    )
+
+
+def get_equilibration_boundaries(equilibration_status: pd.Series) -> pd.DataFrame:
+    """
+        Parse a list of timestamped equilibration statuses into a DataFrame of start
+        and end times of equilibrated states. Assumes all time between unequilibrated
+        and equilibrated input points are unequilibrated.
+
+        Args:
+            equilibration_status: A Series of equilibration status strings.
+        Returns:
+            DataFrame of start and end times of equilibrated states.
+    """
+    # Get a boolean series with timestamped index of equilbrated data points
+    equilibrated_mask = equilibration_status == "equilibrated"
+    leading_edges = equilibrated_mask[equilibrated_mask.astype(int).diff() == 1].index
+    trailing_edges = equilibrated_mask[
+        equilibrated_mask.astype(int).diff(periods=-1) == 1
+    ].index
+
+    # Correct single point ranges to have leading and falling edges
+    if (
+        len(trailing_edges)
+        and len(leading_edges)
+        and trailing_edges[0] < leading_edges[0]
+    ):
+        # Prepend a trailing edge at the start of the dataset as its own leading edge
+        leading_edges = trailing_edges[:1].append(leading_edges)
+
+    if len(trailing_edges) > len(leading_edges):
+        # use the first trailing edge as its own leading edge
+        leading_edges = leading_edges.append(trailing_edges[:1])
+    if len(leading_edges) > len(trailing_edges):
+        # use the final leading edge as its own trailing edge
+        trailing_edges = trailing_edges.append(leading_edges[-1:])
+
+    return pd.DataFrame({"start_time": leading_edges, "end_time": trailing_edges})
+
+
+def pivot_process_experiment_results_on_ROI(
+    experiment_df: pd.DataFrame,
+    ROI_names: List[str] = None,
+    msorm_types: List[str] = None,
+) -> pd.DataFrame:
+    """
+        Flatten a DataFrame of process experiment results down to one row per image.
+
+        Args:
+            experiment_df: A DataFrame of process experiment summary statistics.
+            ROI_names: Optional. A list of ROI names to select. Defaults to all ROIs present.
+            msorm_types: Optional. A list of MSORM column names to select. Defaults to all RGB channel MSORMs.
+        Returns:
+            DataFrame with one row per image, and msorm values for a subset of ROIs. Creates one column
+            for every unique ROI in the dataset. NaN values are used when an image is missing an ROI.
+    """
+    if msorm_types is None:
+        msorm_types = ["r_msorm", "g_msorm", "b_msorm"]
+
+    if ROI_names:
+        experiment_df = experiment_df[experiment_df["ROI"].isin(ROI_names)]
+
+    # Before pivot
+    # image     ROI             r_msorm     timestamp
+    #     1     DO patch             .4     2019-01-01
+    #     1     Reference Patch      .5     2019-01-01
+
+    # Pivot creates a heirachical multiindex data frame with an index for each 'values' column
+    pivot = experiment_df.pivot(
+        index="timestamp", columns="ROI", values=msorm_types + ["image"]
+    )
+
+    # After pivot -> Multi-level index
+    #              DO Patch                 Reference Patch
+    # timestamp    image     r_msorm...     image   r_msorm...
+    # 2019-01-01       1          .4            1        .5
+
+    # There's one copy of the image name for each ROI due to the pivot, so pull out the top level index
+    images = pivot["image"][experiment_df["ROI"].values[0]]
+    pivot.drop("image", axis=1, inplace=True)
+    # Flatten the pivot table index, and add images back in
+    pivot.columns = [
+        # ("r_msorm", "some ROI name") -> "some ROI name r_msorm"
+        " ".join(col[::-1]).strip()
+        for col in pivot.columns.values
+    ]
+    pivot["image"] = images
+
+    # After flattening
+    # timestamp    image     DO Patch r_msorm   Reference Patch r_msorm...
+    # 2019-01-01       1                   .4                        .5
+
+    return pivot
+
+
+def open_and_combine_process_experiment_results(
+    process_experiment_result_filepaths: List[str],
+    ROI_names: List[str] = None,
+    msorm_types: List[str] = None,
+) -> pd.DataFrame:
+    """
+        Open multiple process experiment result files and combine into a single DataFrame with one row per image.
+
+        Args:
+            process_experiment_result_filepaths: A list of filepaths to process experiment summary statistics files.
+            ROI_names: Optional. A list of ROI names to select. Defaults to all ROIs present.
+            msorm_types: Optional. A list of MSORM column names to select. Defaults to all RGB channel MSORMs.
+        Returns:
+            DataFrame of all summary statistics flattened to one row per image with all selected ROIs.
+    """
+    if msorm_types is None:
+        msorm_types = ["r_msorm", "g_msorm", "b_msorm"]
+
+    all_roi_data = pd.concat(
+        [
+            pd.read_csv(results_filepath, parse_dates=["timestamp"])
+            for results_filepath in process_experiment_result_filepaths
+        ]
+    )
+
+    return pivot_process_experiment_results_on_ROI(all_roi_data, ROI_names, msorm_types)
+
+
+def get_all_experiment_images(
+    local_sync_directory: str, experiment_names: List[str]
+) -> pd.DataFrame:
+    """
+        Get a DataFrame of all image files across multiple experiment data directories.
+
+        Args:
+            local_sync_directory: The local data directory, usually ~/osmo/cosmobot-data-sets
+            experiment_names: A list of experiment directory names in the local sync directory.
+        Returns:
+            DataFrame of all image file names and the corresponding experiment name.
+    """
+    all_images = pd.DataFrame(
+        [
+            {"experiment": experiment_name, "image": image}
+            for experiment_name in experiment_names
+            for image in os.listdir(os.path.join(local_sync_directory, experiment_name))
+        ],
+        # Ensure correct dtype and column names when no images are found
+        columns=["experiment", "image"],
+        dtype="object",
+    )
+
+    # Filter out experiment log files
+    return all_images.drop(all_images[~all_images["image"].str.contains("jpeg")].index)
+
+
+def filter_equilibrated_images(equilibration_range: pd.Series, df: pd.DataFrame):
+    """
+        Filter a datetime indexed DataFrame to a given equilibration range
+
+        Args:
+            equilibration_range: A Series with indexed start_time and end_time values
+            df: A datetime indexed DataFrame
+        Returns:
+            A filtered df containing only values whose index is between the given start_time and end_tim.
+    """
+    leading_edge_mask = df.index > equilibration_range["start_time"]
+    trailing_edge_mask = df.index < equilibration_range["end_time"]
+    return df[leading_edge_mask & trailing_edge_mask]
+
+
+def open_and_combine_and_filter_source_data(
+    local_sync_directory: str,
+    experiment_names: List[str],
+    calibration_log_filepaths: List[str],
+    picolog_log_filepaths: List[str],
+    process_experiment_result_filepaths: List[str],
+    ROI_names: List[str] = None,
+    msorm_types: List[str] = None,
+) -> pd.DataFrame:
+    """
+        Combine and filter a collection of calibration environment, PicoLog, process experiment and image data
+        into a neat DataFrame of all images taken during periods of equilibrated temperature and dissolved oxygen.
+
+        Args:
+            local_sync_directory: The local data directory, usually ~/osmo/cosmobot-data-sets
+            experiment_names: A list of experiment directory names in the local sync directory.
+            calibration_log_filepaths: A list of filepaths to calibration environment csv data log files.
+            picolog_log_filepaths: A list of filepaths to PicoLog csv data files.
+            process_experiment_result_filepaths: A list of filepaths to process experiment summary statistics files.
+            ROI_names: Optional. A list of ROI names to select. Defaults to all ROIs present.
+            msorm_types: Optional. A list of MSORM column names to select. Defaults to all RGB channel MSORMs.
+
+        Returns:
+            DataFrame of image and sensor data collected during equilibrated states.
+    """
+    if msorm_types is None:
+        msorm_types = ["r_msorm", "g_msorm", "b_msorm"]
+
+    all_sensor_data = open_and_combine_picolog_and_calibration_data(
+        calibration_log_filepaths, picolog_log_filepaths
+    )
+
+    equilibration_boundaries = get_equilibration_boundaries(
+        all_sensor_data["equilibration status"]
+    )
+
+    all_roi_data = open_and_combine_process_experiment_results(
+        process_experiment_result_filepaths, ROI_names, msorm_types
+    )
+
+    equilibrated_data = pd.concat(
+        equilibration_boundaries.apply(
+            filter_equilibrated_images, axis=1, df=all_roi_data
+        ).values
+    ).sort_index()
+
+    all_images = get_all_experiment_images(local_sync_directory, experiment_names)
+
+    return (
+        equilibrated_data.reset_index()
+        .set_index("image")
+        .join(all_images.set_index("image"), how="inner")
+    )
---
+++ osmo_jupyter/dataset_test.py
@@ -0,0 +1,421 @@
+import io
+
+import pandas as pd
+import pytest
+
+import osmo_jupyter.dataset as module
+
+TEST_PICOLOG_DATA = pd.DataFrame(
+    [
+        {"timestamp": "2019-01-01T00:00:00-07:00", "Temperature Ave. (C)": 39},
+        {"timestamp": "2019-01-01T00:00:02-07:00", "Temperature Ave. (C)": 40},
+        {"timestamp": "2019-01-01T00:00:04-07:00", "Temperature Ave. (C)": 40},
+    ]
+)
+
+TEST_CALIBRATION_DATA = pd.DataFrame(
+    [
+        {"timestamp": "2019-01-01 00:00:00.1", "equilibration status": "waiting"},
+        {"timestamp": "2019-01-01 00:00:01.1", "equilibration status": "equilibrated"},
+        {"timestamp": "2019-01-01 00:00:03.1", "equilibration status": "equilibrated"},
+        {"timestamp": "2019-01-01 00:00:04.1", "equilibration status": "waiting"},
+    ]
+)
+
+
+TEST_PROCESS_EXPERIMENT_DATA = pd.DataFrame(
+    [
+        {
+            "timestamp": pd.to_datetime("2019-01-01 00:00:00"),
+            "image": "image-0.jpeg",
+            "ROI": "ROI 0",
+            "r_msorm": 0.5,
+            "g_msorm": 0.4,
+        },
+        {
+            "timestamp": pd.to_datetime("2019-01-01 00:00:00"),
+            "image": "image-0.jpeg",
+            "ROI": "ROI 1",
+            "r_msorm": 0.4,
+            "g_msorm": 0.5,
+        },
+        {
+            "timestamp": pd.to_datetime("2019-01-01 00:00:02"),
+            "image": "image-1.jpeg",
+            "ROI": "ROI 0",
+            "r_msorm": 0.3,
+            "g_msorm": 0.6,
+        },
+        {
+            "timestamp": pd.to_datetime("2019-01-01 00:00:02"),
+            "image": "image-1.jpeg",
+            "ROI": "ROI 1",
+            "r_msorm": 0.6,
+            "g_msorm": 0.3,
+        },
+    ]
+)
+
+
+@pytest.fixture
+def mock_picolog_file_obj():
+    test_picolog_file = io.StringIO()
+    TEST_PICOLOG_DATA.to_csv(test_picolog_file, index=False)
+    test_picolog_file.seek(0)
+
+    return test_picolog_file
+
+
+@pytest.fixture
+def mock_calibration_file_obj():
+    test_calibration_env_file = io.StringIO()
+    TEST_CALIBRATION_DATA.to_csv(test_calibration_env_file, index=False)
+    test_calibration_env_file.seek(0)
+
+    return test_calibration_env_file
+
+
+class TestOpenAndCombineSensorData:
+    def test_parses_picolog_timestamps_correctly(self, mock_picolog_file_obj):
+        picolog_data = module.open_picolog_file(mock_picolog_file_obj)
+
+        assert len(picolog_data)
+        assert picolog_data.index[0] == pd.to_datetime("2019-01-01 00:00:00")
+
+    def test_parses_calibration_log_timestampes_correctly(
+        self, mock_calibration_file_obj
+    ):
+        calibration_log_data = module.open_calibration_log_file(
+            mock_calibration_file_obj
+        )
+
+        assert len(calibration_log_data)
+        assert calibration_log_data.index[0] == pd.to_datetime("2019-01-01 00:00:00")
+
+    def test_interpolates_data_correctly(
+        self, mock_calibration_file_obj, mock_picolog_file_obj
+    ):
+        combined_data = module.open_and_combine_picolog_and_calibration_data(
+            calibration_log_filepaths=[mock_calibration_file_obj],
+            picolog_log_filepaths=[mock_picolog_file_obj],
+        ).reset_index()  # move timestamp index to a column
+
+        expected_interpolation = pd.DataFrame(
+            [
+                {
+                    "timestamp": "2019-01-01 00:00:00",
+                    "equilibration status": "waiting",
+                    "PicoLog Temperature Ave. (C)": 39,
+                },
+                {
+                    "timestamp": "2019-01-01 00:00:01",
+                    "equilibration status": "equilibrated",
+                    "PicoLog Temperature Ave. (C)": 39.5,
+                },
+                {
+                    "timestamp": "2019-01-01 00:00:03",
+                    "equilibration status": "equilibrated",
+                    "PicoLog Temperature Ave. (C)": 40,
+                },
+                {
+                    "timestamp": "2019-01-01 00:00:04",
+                    "equilibration status": "waiting",
+                    "PicoLog Temperature Ave. (C)": 40,
+                },
+            ]
+        ).astype(
+            combined_data.dtypes
+        )  # coerce datatypes to match
+
+        pd.testing.assert_frame_equal(combined_data, expected_interpolation)
+
+
+class TestGetEquilibrationBoundaries:
+    @pytest.mark.parametrize(
+        "input_equilibration_status, expected_boundaries",
+        [
+            (
+                {  # Use full timestamps to show that it works at second resolution
+                    pd.to_datetime("2019-01-01 00:00:00"): "waiting",
+                    pd.to_datetime("2019-01-01 00:00:01"): "equilibrated",
+                    pd.to_datetime("2019-01-01 00:00:02"): "equilibrated",
+                    pd.to_datetime("2019-01-01 00:00:03"): "waiting",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2019-01-01 00:00:01"),
+                        "end_time": pd.to_datetime("2019-01-01 00:00:02"),
+                    }
+                ],
+            ),
+            (
+                {  # Switch to using only years as the timestamp for terseness and readability
+                    pd.to_datetime("2019"): "waiting",
+                    pd.to_datetime("2020"): "equilibrated",
+                    pd.to_datetime("2021"): "waiting",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2020"),
+                        "end_time": pd.to_datetime("2020"),
+                    }
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2020"): "equilibrated",
+                    pd.to_datetime("2021"): "waiting",
+                    pd.to_datetime("2022"): "equilibrated",
+                    pd.to_datetime("2023"): "waiting",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2020"),
+                        "end_time": pd.to_datetime("2020"),
+                    },
+                    {
+                        "start_time": pd.to_datetime("2022"),
+                        "end_time": pd.to_datetime("2022"),
+                    },
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2019"): "waiting",
+                    pd.to_datetime("2020"): "equilibrated",
+                    pd.to_datetime("2021"): "waiting",
+                    pd.to_datetime("2022"): "equilibrated",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2020"),
+                        "end_time": pd.to_datetime("2020"),
+                    },
+                    {
+                        "start_time": pd.to_datetime("2022"),
+                        "end_time": pd.to_datetime("2022"),
+                    },
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2019"): "waiting",
+                    pd.to_datetime("2020"): "equilibrated",
+                    pd.to_datetime("2021"): "waiting",
+                    pd.to_datetime("2022"): "equilibrated",
+                    pd.to_datetime("2023"): "waiting",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2020"),
+                        "end_time": pd.to_datetime("2020"),
+                    },
+                    {
+                        "start_time": pd.to_datetime("2022"),
+                        "end_time": pd.to_datetime("2022"),
+                    },
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2019"): "equilibrated",
+                    pd.to_datetime("2020"): "waiting",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2019"),
+                        "end_time": pd.to_datetime("2019"),
+                    }
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2019"): "waiting",
+                    pd.to_datetime("2020"): "equilibrated",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2020"),
+                        "end_time": pd.to_datetime("2020"),
+                    }
+                ],
+            ),
+            (
+                {
+                    pd.to_datetime("2019"): "equilibrated",
+                    pd.to_datetime("2020"): "waiting",
+                    pd.to_datetime("2021"): "equilibrated",
+                },
+                [
+                    {
+                        "start_time": pd.to_datetime("2019"),
+                        "end_time": pd.to_datetime("2019"),
+                    },
+                    {
+                        "start_time": pd.to_datetime("2021"),
+                        "end_time": pd.to_datetime("2021"),
+                    },
+                ],
+            ),
+        ],
+    )
+    def test_finds_correct_edges(self, input_equilibration_status, expected_boundaries):
+
+        parsed_equilibration_boundaries = module.get_equilibration_boundaries(
+            equilibration_status=pd.Series(input_equilibration_status)
+        )
+
+        expected_equilibration_boundaries = pd.DataFrame(
+            expected_boundaries,
+            columns=["start_time", "end_time"],
+            dtype="datetime64[ns]",
+        ).reset_index(
+            drop=True
+        )  # Coerce to a RangeIndex when creating empty DataFrame
+
+        pd.testing.assert_frame_equal(
+            parsed_equilibration_boundaries, expected_equilibration_boundaries
+        )
+
+
+class TestPivotProcessExperimentResults:
+    def test_combines_image_rows_by_ROI(self):
+        pivot_results = module.pivot_process_experiment_results_on_ROI(
+            experiment_df=TEST_PROCESS_EXPERIMENT_DATA,
+            ROI_names=list(TEST_PROCESS_EXPERIMENT_DATA["ROI"].unique()),
+            msorm_types=["r_msorm", "g_msorm"],
+        )
+
+        expected_results_data = (
+            pd.DataFrame(
+                [
+                    {
+                        "timestamp": pd.to_datetime("2019-01-01 00:00:00"),
+                        "ROI 0 r_msorm": 0.5,
+                        "ROI 1 r_msorm": 0.4,
+                        "ROI 0 g_msorm": 0.4,
+                        "ROI 1 g_msorm": 0.5,
+                        "image": "image-0.jpeg",
+                    },
+                    {
+                        "timestamp": pd.to_datetime("2019-01-01 00:00:02"),
+                        "ROI 0 r_msorm": 0.3,
+                        "ROI 1 r_msorm": 0.6,
+                        "ROI 0 g_msorm": 0.6,
+                        "ROI 1 g_msorm": 0.3,
+                        "image": "image-1.jpeg",
+                    },
+                ]
+            )
+            .set_index("timestamp")
+            .astype(pivot_results.dtypes)
+        )
+
+        pd.testing.assert_frame_equal(pivot_results, expected_results_data)
+
+
+class TestGetAllExperimentImages:
+    def test_returns_only_image_files(self, mocker):
+        image_file_name = "image-0.jpeg"
+        experiment_name = "test"
+
+        mocker.patch("os.listdir", return_value=[image_file_name, "experiment.log"])
+
+        experiment_images = module.get_all_experiment_images(
+            local_sync_directory="", experiment_names=[experiment_name]
+        )
+
+        expected_images = pd.DataFrame(
+            [{"experiment": experiment_name, "image": image_file_name}]
+        )
+
+        pd.testing.assert_frame_equal(experiment_images, expected_images)
+
+    def test_has_correct_dtype_when_no_images_found(self, mocker):
+        mocker.patch("os.listdir", return_value=[])
+
+        experiment_images = module.get_all_experiment_images(
+            local_sync_directory="", experiment_names=["test"]
+        )
+
+        pd.testing.assert_frame_equal(
+            experiment_images,
+            pd.DataFrame(columns=["experiment", "image"], dtype="object"),
+        )
+
+
+class TestFilterEquilibratedImages:
+    def test_returns_only_equilibrated_images(self, mock_calibration_file_obj):
+        test_roi_data = pd.DataFrame(
+            [
+                {"timestamp": pd.to_datetime("2019-01-01"), "image": "image-0.jpeg"},
+                {"timestamp": pd.to_datetime("2019-01-03"), "image": "image-1.jpeg"},
+            ]
+        ).set_index("timestamp")
+
+        test_equilibration_boundaries = pd.Series(
+            {
+                "start_time": pd.to_datetime("2019-01-02"),
+                "end_time": pd.to_datetime("2019-01-04"),
+            }
+        )
+
+        equilibrated_image_data = module.filter_equilibrated_images(
+            equilibration_range=test_equilibration_boundaries, df=test_roi_data
+        )
+
+        expected_equilibrated_image_data = test_roi_data[1:]
+
+        pd.testing.assert_frame_equal(
+            equilibrated_image_data, expected_equilibrated_image_data
+        )
+
+
+class TestOpenAndCombineSourceData:
+    def test_filters_all_data_to_equilibrated_states(
+        self, mocker, mock_calibration_file_obj, mock_picolog_file_obj
+    ):
+        test_files = ["image-0.jpeg", "image-1.jpeg", "experiment.log"]
+        mocker.patch("os.listdir", return_value=test_files)
+
+        mocker.patch.object(
+            module,
+            "open_and_combine_process_experiment_results",
+            return_value=module.pivot_process_experiment_results_on_ROI(
+                experiment_df=TEST_PROCESS_EXPERIMENT_DATA,
+                ROI_names=list(TEST_PROCESS_EXPERIMENT_DATA["ROI"].unique()),
+                msorm_types=["r_msorm", "g_msorm"],
+            ),
+        )
+
+        experiment_name = "test"
+
+        equilibrated_experiment_data = module.open_and_combine_and_filter_source_data(
+            local_sync_directory="",
+            experiment_names=[experiment_name],
+            calibration_log_filepaths=[mock_calibration_file_obj],
+            picolog_log_filepaths=[mock_picolog_file_obj],
+            process_experiment_result_filepaths=[],
+        )
+
+        expected_experiment_data = (
+            pd.DataFrame(
+                [
+                    {
+                        "timestamp": pd.to_datetime("2019-01-01 00:00:02"),
+                        "ROI 0 r_msorm": 0.3,
+                        "ROI 1 r_msorm": 0.6,
+                        "ROI 0 g_msorm": 0.6,
+                        "ROI 1 g_msorm": 0.3,
+                        "image": "image-1.jpeg",
+                        "experiment": experiment_name,
+                    }
+                ]
+            )
+            .set_index("image")
+            .astype(equilibrated_experiment_data.dtypes)
+        )
+
+        pd.testing.assert_frame_equal(
+            equilibrated_experiment_data, expected_experiment_data
+        )
