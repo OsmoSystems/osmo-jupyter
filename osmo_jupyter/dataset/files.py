e8aafc694d4d63eeaf53bdc4d4c871c7c6418237
thatneat <thatneat@gmail.com>
2019-11-05 10:26:42 -0800

    Get experiment data files from Google Drive format

---
+++ osmo_jupyter/dataset/files.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+
+FILE_TYPES = [
+    "setpoints",
+    "calibration_log",
+    "pico",
+    "process_experiment",
+    "ysi_prodo",
+    "ysi_prosolo",
+    "summary_movies",
+]
+
+DATA_DIRECTORY_NAME = "data"
+
+
+def _get_experiment_data_file_paths_for_type(project_directory, file_type):
+    subdirectory_path = os.path.join(project_directory, DATA_DIRECTORY_NAME, file_type)
+
+    if not os.path.exists(subdirectory_path):
+        return []
+
+    files_and_folders_in_subdirectory = [
+        os.path.join(subdirectory_path, filename)
+        for filename in os.listdir(subdirectory_path)
+    ]
+    files_in_subdirectory = [
+        filepath
+        for filepath in files_and_folders_in_subdirectory
+        if os.path.isfile(filepath)
+    ]
+
+    return files_in_subdirectory
+
+
+def get_experiment_data_files_by_type(project_directory):
+    """ Spider the provided directory for files related to an experiment.
+    Expects the project directory to match our standard Google Drive experiment directory format.
+
+    Args:
+        project_directory: Google Drive experiment directory containing all of
+            the data files for this data collection attempt
+    Returns:
+        pandas Series, indexed by file type and containing lists of filenames.
+    """
+
+    return pd.Series(
+        {
+            file_type: _get_experiment_data_file_paths_for_type(
+                project_directory, file_type
+            )
+            for file_type in FILE_TYPES
+        }
+    )
---
+++ osmo_jupyter/dataset/files_test.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+
+import osmo_jupyter.dataset.files as module
+
+
+def _init_data_dir(parent_dir, directories_to_include, files_to_include):
+    """set up a data directory with folders and files in it
+    Args:
+        directories_to_include: iterable of directory names to initialize
+        files_to_include: iterable of file names to initialize. If they are in subdirectories,
+            the subdirectories should be listed in directories_to_include to be created first
+    """
+    data_dir = os.path.join(parent_dir, "data")
+    os.mkdir(data_dir)
+
+    for relative_directory_path in directories_to_include:
+        os.makedirs(os.path.join(data_dir, relative_directory_path))
+
+    for relative_file_path in files_to_include:
+        with open(os.path.join(data_dir, relative_file_path), "w"):
+            # Just opening the file is enough to create it.
+            pass
+
+
+class TestGetExperimentDataFilePathsForType:
+    def test_no_directory_returns_empty(self, tmp_path):
+        actual = module._get_experiment_data_file_paths_for_type(
+            tmp_path, "this does NOT exist as a subdirectory"
+        )
+        expected = []
+        assert actual == expected
+
+    def test_gets_file_from_subdirectory_and_ignores_sub_subdirectories(self, tmp_path):
+        _init_data_dir(
+            tmp_path,
+            directories_to_include=["ysi_prodo/subdirectory"],
+            files_to_include=["ysi_prodo/somethin"],
+        )
+        actual = module._get_experiment_data_file_paths_for_type(tmp_path, "ysi_prodo")
+        expected = [os.path.join(tmp_path, "data/ysi_prodo/somethin")]
+        assert actual == expected
+
+    def test_gets_multiple_files(self, tmp_path):
+        relative_filepaths = [
+            os.path.join("ysi_prodo", "somethin"),
+            os.path.join("ysi_prodo", "somethin2"),
+        ]
+
+        _init_data_dir(
+            tmp_path,
+            directories_to_include=["ysi_prodo"],
+            files_to_include=relative_filepaths,
+        )
+
+        expected = [
+            os.path.join(tmp_path, "data", relative_filepath)
+            for relative_filepath in relative_filepaths
+        ]
+
+        actual = module._get_experiment_data_file_paths_for_type(tmp_path, "ysi_prodo")
+        assert actual == expected
+
+
+class TestGetExperimentDataFilesByType:
+    def test_returns_series_with_full_file_paths_and_empty_keys(self, tmp_path):
+        _init_data_dir(
+            tmp_path, ["ysi_prosolo", "ysi_prodo"], ["ysi_prosolo/KorDSS file.csv"]
+        )
+
+        expected = pd.Series(
+            {
+                "setpoints": [],
+                "calibration_log": [],
+                "pico": [],
+                "process_experiment": [],
+                "ysi_prodo": [],
+                "ysi_prosolo": [
+                    os.path.join(tmp_path, "data", "ysi_prosolo", "KorDSS file.csv")
+                ],
+                "summary_movies": [],
+            }
+        )
+
+        pd.testing.assert_series_equal(
+            module.get_experiment_data_files_by_type(tmp_path), expected
+        )
