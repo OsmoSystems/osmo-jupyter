cf01020aefcebaed5fe35d1119412a416b5e1325
thatneat <thatneat@users.noreply.github.com>
2019-11-06 13:16:11 -0800

    Get experiment data files from Google Drive format (#38)
    * Get experiment data files from Google Drive format
    
    * compensate for inconsistent os.listdir ordering
    
    * per Jacob's correction, it's ProODO as in professional optical DO I guess
    
    * uhhh tests
    
    * PR feedback: module rename, plus stoked about `pathlib`
    
    * let's be even clearer here, shan't we
    
    * trailing whitespace
    
    * words
    
    * go all-in

---
+++ osmo_jupyter/dataset/source_files.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+import pandas as pd
+
+"""
+In our Google Drive folder structure, each data collection event has a top-level folder. The structured data files
+are all in a /data folder underneath that, organized in subfolders by data type.
+The /data/setpoints folder will have all of the setpoints files, PicoLog files are under /data/pico, etc.
+
+Note that based on the nature of a particular data collection event, some subfolders may not be present.
+
+Example Google Drive folder:
+/data/Google Drive/Technical/Experiments/2019-08-23 Sweeps Attempt 11/
+├── 2019-08-23 README - Collect Sweeps Attempt 11.gddoc
+└── data
+    ├── calibration_log
+    │   └── 2019-08-23--12-37-43_calibration.csv
+    ├── pico
+    │   └── 2019-08-23_attempt_11_pico.csv
+    ├── process_experiment
+    │   └── 2019-08-23--13-10-40-[...] - summary statistics (generated 2019-08-27--10-34-46).csv
+    ├── setpoints
+    │   └── 2019-08-16--14-32-14_two_sweeps_setpoints_1_with_extra.csv
+    └── summary_movies
+        └── 2019-08-23--13-10-40-Pi2E32-sweeps_attempt_11_1566589063 - equilibrated images summary.mp4
+
+Official docs:
+https://docs.google.com/document/d/1ri0LNyxWqtZ5g05T7o2pd_VQp3-ndKRVRT1TjiwnnZY/edit#heading=h.hx6dx98ojbtg
+"""
+DATA_DIRECTORY_NAME = "data"
+
+FILE_TYPE_SUBFOLDERS = [
+    "setpoints",
+    "calibration_log",
+    "pico",
+    "process_experiment",
+    "ysi_proodo",
+    "ysi_prosolo",
+    "summary_movies",
+]
+
+
+def _get_experiment_data_file_paths_for_type(project_directory, file_type):
+    subdirectory_path = Path(project_directory) / DATA_DIRECTORY_NAME / file_type
+
+    if not subdirectory_path.exists():
+        return []
+
+    files_in_subdirectory = sorted(
+        filepath for filepath in subdirectory_path.iterdir() if filepath.is_file()
+    )
+
+    return files_in_subdirectory
+
+
+def get_experiment_data_files_by_type(project_directory):
+    """ Spider the provided directory for files related to an experiment.
+    Expects the project directory to match our standard Google Drive experiment directory format.
+
+    Args:
+        project_directory: Google Drive experiment directory containing all of
+            the data files for this data collection attempt
+    Returns:
+        pandas Series, indexed by file type and containing lists of filenames as Path objects.
+    """
+
+    return pd.Series(
+        {
+            file_type: _get_experiment_data_file_paths_for_type(
+                project_directory, file_type
+            )
+            for file_type in FILE_TYPE_SUBFOLDERS
+        }
+    )
---
+++ osmo_jupyter/dataset/source_files_test.py
@@ -0,0 +1,88 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+
+import osmo_jupyter.dataset.source_files as module
+
+
+def _init_data_dir(parent_dir: Path, directories_to_include, files_to_include):
+    """set up a data directory with folders and files in it
+    Args:
+        parent_dir: Path object that everything's expected to be under
+        directories_to_include: iterable of directory names to initialize
+        files_to_include: iterable of file names to initialize. If they are in subdirectories,
+            the subdirectories should be listed in directories_to_include to be created first
+    """
+    data_dir = parent_dir / "data"
+    data_dir.mkdir()
+
+    for relative_directory_path in directories_to_include:
+        (data_dir / relative_directory_path).mkdir(parents=True)
+
+    for relative_file_path in files_to_include:
+        (data_dir / relative_file_path).touch()
+
+
+class TestGetExperimentDataFilePathsForType:
+    def test_no_directory_returns_empty(self, tmp_path):
+        actual = module._get_experiment_data_file_paths_for_type(
+            tmp_path, "this does NOT exist as a subdirectory"
+        )
+        expected = []
+        assert actual == expected
+
+    def test_gets_file_from_subdirectory_and_ignores_sub_subdirectories(self, tmp_path):
+        _init_data_dir(
+            tmp_path,
+            directories_to_include=[Path("ysi_proodo") / "subdirectory"],
+            files_to_include=[Path("ysi_proodo") / "somethin.csv"],
+        )
+        actual = module._get_experiment_data_file_paths_for_type(tmp_path, "ysi_proodo")
+        expected = [tmp_path / "data" / "ysi_proodo" / "somethin.csv"]
+        assert actual == expected
+
+    def test_gets_multiple_files(self, tmp_path):
+        relative_filepaths = [
+            os.path.join("ysi_proodo", "somethin.csv"),
+            os.path.join("ysi_proodo", "somethin2.csv"),
+        ]
+
+        _init_data_dir(
+            tmp_path,
+            directories_to_include=["ysi_proodo"],
+            files_to_include=relative_filepaths,
+        )
+
+        expected = [
+            tmp_path / "data" / relative_filepath
+            for relative_filepath in relative_filepaths
+        ]
+
+        actual = module._get_experiment_data_file_paths_for_type(tmp_path, "ysi_proodo")
+        assert actual == expected
+
+
+class TestGetExperimentDataFilesByType:
+    def test_returns_series_with_full_file_paths_and_empty_keys(self, tmp_path):
+        _init_data_dir(
+            tmp_path,
+            ["ysi_prosolo", "ysi_proodo"],
+            [Path("ysi_prosolo") / "KorDSS file.csv"],
+        )
+
+        expected = pd.Series(
+            {
+                "setpoints": [],
+                "calibration_log": [],
+                "pico": [],
+                "process_experiment": [],
+                "ysi_proodo": [],
+                "ysi_prosolo": [tmp_path / "data" / "ysi_prosolo" / "KorDSS file.csv"],
+                "summary_movies": [],
+            }
+        )
+
+        pd.testing.assert_series_equal(
+            module.get_experiment_data_files_by_type(tmp_path), expected
+        )
