37720fcc2f7dc1f3bdbb54ec93d02aae5aad272d
thatneat <thatneat@gmail.com>
2019-11-05 16:42:10 -0800

    let's be even clearer here, shan't we

--- osmo_jupyter/dataset/source_files.py
+++ osmo_jupyter/dataset/source_files.py
@@ -6,6 +6,24 @@ import pandas as pd
 In our Google Drive folder structure, each data collection event has a top-level folder. The structured data files
 are all in a /data folder underneath that, organized in subfolders by data type.
 The /data/setpoints folder will have all of the setpoints files, PicoLog files are under /data/pico, etc.
+
+Note that based on the nature of the data collection, some data types may not be present in a particular folder. 
+
+Example Google Drive folder:
+/data/Google Drive/Technical/Experiments/2019-08-23 Sweeps Attempt 11/
+├── 2019-08-23 README - Collect Sweeps Attempt 11.gddoc
+└── data
+    ├── calibration_log
+    │   └── 2019-08-23--12-37-43_calibration.csv
+    ├── pico
+    │   └── 2019-08-23_attempt_11_pico.csv
+    ├── process_experiment
+    │   └── 2019-08-23--13-10-40-[...] - summary statistics (generated 2019-08-27--10-34-46).csv
+    ├── setpoints
+    │   └── 2019-08-16--14-32-14_two_sweeps_setpoints_1_with_extra.csv
+    └── summary_movies
+        └── 2019-08-23--13-10-40-Pi2E32-sweeps_attempt_11_1566589063 - equilibrated images summary.mp4
+
 Official docs:
 https://docs.google.com/document/d/1ri0LNyxWqtZ5g05T7o2pd_VQp3-ndKRVRT1TjiwnnZY/edit#heading=h.hx6dx98ojbtg
 """
