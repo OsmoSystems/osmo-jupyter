ebe80b06e90cb3ae594f99710a03f480fa95df2c
thatneat <thatneat@gmail.com>
2019-11-06 09:24:35 -0800

    go all-in

--- osmo_jupyter/dataset/source_files_test.py
+++ osmo_jupyter/dataset/source_files_test.py
@@ -35,11 +35,11 @@ class TestGetExperimentDataFilePathsForType:
     def test_gets_file_from_subdirectory_and_ignores_sub_subdirectories(self, tmp_path):
         _init_data_dir(
             tmp_path,
-            directories_to_include=["ysi_proodo/subdirectory"],
-            files_to_include=["ysi_proodo/somethin.csv"],
+            directories_to_include=[Path("ysi_proodo") / "subdirectory"],
+            files_to_include=[Path("ysi_proodo") / "somethin.csv"],
         )
         actual = module._get_experiment_data_file_paths_for_type(tmp_path, "ysi_proodo")
-        expected = [tmp_path / "data/ysi_proodo/somethin.csv"]
+        expected = [tmp_path / "data" / "ysi_proodo" / "somethin.csv"]
         assert actual == expected
 
     def test_gets_multiple_files(self, tmp_path):
@@ -66,7 +66,9 @@ class TestGetExperimentDataFilePathsForType:
 class TestGetExperimentDataFilesByType:
     def test_returns_series_with_full_file_paths_and_empty_keys(self, tmp_path):
         _init_data_dir(
-            tmp_path, ["ysi_prosolo", "ysi_proodo"], ["ysi_prosolo/KorDSS file.csv"]
+            tmp_path,
+            ["ysi_prosolo", "ysi_proodo"],
+            [Path("ysi_prosolo") / "KorDSS file.csv"],
         )
 
         expected = pd.Series(
@@ -76,7 +78,7 @@ class TestGetExperimentDataFilesByType:
                 "pico": [],
                 "process_experiment": [],
                 "ysi_proodo": [],
-                "ysi_prosolo": [tmp_path / "data/ysi_prosolo/KorDSS file.csv"],
+                "ysi_prosolo": [tmp_path / "data" / "ysi_prosolo" / "KorDSS file.csv"],
                 "summary_movies": [],
             }
         )
